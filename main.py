b0a302cb00e413a941ca645d7992f9f5bedc5b17
Lahari-062 <ramalahari89@gmail.com>
2026-03-16 20:18:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-from ocr_module import extract_text_from_image
+from extraction_module import extract_health_data
+from ml_module import analyze_health
+from diet_generator import generate_diet
+from chatbot_module import chatbot_response
 
-file_path = "sample_report.png"
+sample_text = "Glucose: 140 Cholesterol: 220 BMI: 27"
 
-text = extract_text_from_image(file_path)
+data = extract_health_data(sample_text)
 
-print("Extracted Medical Report Text:")
-print(text)
+condition = analyze_health(data)
+
+diet = generate_diet(condition)
+
+advice = chatbot_response(condition)
+
+print("Health Data:", data)
+print("Condition:", condition)
+print("Diet Plan:", diet)
+print("Chatbot Advice:", advice)
